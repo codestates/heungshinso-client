0eed772974f48c90402d81f51ee4afd8c3d1aef0
Ranisol <gim883364@gmail.com>
2020-11-18 14:35:10 +0900

    Add css

--- src/components/nav/signhandle/signin.js
+++ src/components/nav/signhandle/signin.js
@@ -12,12 +12,9 @@ class SignIn extends Component {
       email: '',
       password: '',
       errorMessage: '',
-      isOpenEmailLogin: false,
     };
   }
-  handleEmailLogin() {
-    this.setState((pres) => ({ isOpenEmailLogin: !pres.isOpenEmailLogin }));
-  }
+
   handleInputValue = (key) => (e) => {
     this.setState({ [key]: e.target.value });
   };
@@ -74,141 +71,108 @@ class SignIn extends Component {
     const state = '12345';
     return (
       <div>
-        {!this.state.isOpenEmailLogin ? (
-          <div>
-            <span
-              className="signhandle_modal"
-              onClick={this.props.signInModalHandler}
-            ></span>
+        <span
+          className="signhandle_modal"
+          onClick={this.props.signInModalHandler}
+        ></span>
 
-            <span className="sign_in_and_out">
-              <span className="signin_intro">
-                <div className="signin_intro_text">흥</div>
-                <div className="signin_intro_text">신</div>
-                <div className="signin_intro_text">소</div>
-              </span>
-              <span className="signin_login">
-                <div className="signin_login_title"></div>
-                <div
-                  className="signin_login_content"
-                  onClick={this.handleEmailLogin.bind(this)}
+        <div className="sign_in_and_out">
+          <div className="signin">
+            <div className="signin_container">
+              <span className="signin_container_item">이메일 로그인</span>
+              <form
+                className="signin_container_item"
+                onSubmit={(e) => {
+                  e.preventDefault();
+                }}
+              >
+                <input
+                  className="signin_container_item_input"
+                  type="email"
+                  placeholder="이메일"
+                  onChange={this.handleInputValue('email')}
+                ></input>
+
+                <input
+                  className="signin_container_item_input"
+                  type="password"
+                  placeholder="비밀번호 숫자, 문자 포함 8자리 이상"
+                  onChange={this.handleInputValue('password')}
+                ></input>
+                {!this.state.email.includes('@') ? (
+                  <div className="signin_container_item_error">
+                    이메일을 입력하세요
+                  </div>
+                ) : this.state.password.length <= 8 ? (
+                  <div className="signin_container_item_input">
+                    비밀번호를 8자 이상 입력하세요
+                  </div>
+                ) : (
+                  <div> {this.state.errorMessage}</div>
+                )}
+                <button
+                  className="signin_container_item_input"
+                  type="submit"
+                  onClick={this.handleSignIn}
                 >
-                  <img
-                    className="signin_login_content_img"
-                    src={emailImg}
-                  ></img>
-                  <a className="signin_login_content_a">이메일 로그인</a>
-                </div>
-                <div className="signin_login_content">
+                  로그인
+                </button>
+              </form>
+            </div>
+            <div className="signin_social_login">
+              <span className="signin_login_content">
+                <a
+                  className="signin_login_content_a"
+                  href="https://github.com/login/oauth/authorize?client_id=67284f7fd9e4bc6602f7&redirect_uri=http://3.35.21.164:3000/users/signin/callback"
+                >
                   <img
                     className="signin_login_content_img"
                     src={githubImg}
                   ></img>
-                  <a
-                    className="signin_login_content_a"
-                    href="https://github.com/login/oauth/authorize?client_id=67284f7fd9e4bc6602f7&redirect_uri=http://3.35.21.164:3000/users/signin/callback"
-                  >
-                    GitHub 로그인
-                  </a>
-                </div>
-                <div className="signin_login_content">
+                </a>
+              </span>
+              <span className="signin_login_content">
+                <a
+                  className="signin_login_content_a"
+                  href={
+                    'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' +
+                    client_id +
+                    '&redirect_uri=' +
+                    redirectURI +
+                    '&state=' +
+                    state
+                  }
+                >
+                  {' '}
                   <img
                     className="signin_login_content_img"
                     src={naverImg}
                   ></img>
-                  <a
-                    className="signin_login_content_a"
-                    href={
-                      'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' +
-                      client_id +
-                      '&redirect_uri=' +
-                      redirectURI +
-                      '&state=' +
-                      state
-                    }
-                  >
-                    {' '}
-                    네이버 로그인
-                  </a>
-                </div>
-                <div className="signin_login_content">
+                </a>
+              </span>
+              <span className="signin_login_content">
+                <a
+                  className="signin_login_content_a"
+                  href="http://3.35.21.164:3000/users/signin/kakaologin"
+                >
                   <img
                     className="signin_login_content_img"
                     src={kakaoImg}
                   ></img>
-                  <a
-                    className="signin_login_content_a"
-                    href="http://3.35.21.164:3000/users/signin/kakaologin"
-                  >
-                    {' '}
-                    카카오 로그인
-                  </a>
-                </div>
+                </a>
               </span>
-            </span>
-          </div>
-        ) : (
-          <div>
-            <span
-              className="signhandle_modal"
-              onClick={this.props.signInModalHandler}
-            ></span>
-            <div className="sign_in_and_out">
-              <div className="signin_email">
-                <div className="signin_email_item">
-                  <div className="signin_email_item_content">이메일 로그인</div>
-                </div>
-                <form
-                  className="signin_email_item"
-                  onSubmit={(e) => {
-                    e.preventDefault();
-                  }}
-                >
-                  <input
-                    className="signin_email_input"
-                    type="email"
-                    placeholder="이메일"
-                    onChange={this.handleInputValue('email')}
-                  ></input>
-
-                  <input
-                    className="signin_email_input"
-                    type="password"
-                    placeholder="비밀번호 숫자, 문자 포함 8자리 이상"
-                    onChange={this.handleInputValue('password')}
-                  ></input>
-                  {!this.state.email.includes('@') ? (
-                    <div className="signin_email_input">
-                      이메일을 입력하세요
-                    </div>
-                  ) : this.state.password.length <= 8 ? (
-                    <div className="signin_email_input">
-                      비밀번호를 8자 이상 입력하세요
-                    </div>
-                  ) : (
-                    <div> {this.state.errorMessage}</div>
-                  )}
-                  <button
-                    className="signin_email_input"
-                    type="submit"
-                    onClick={this.handleSignIn}
-                  >
-                    로그인
-                  </button>
-                </form>
-                <div
-                  className="signin_email_item"
-                  onClick={(event) => {
-                    this.props.signUpModalHandler();
-                    this.props.signInModalHandler();
-                  }}
-                >
-                  아이디가 없으신가요?
-                </div>
-              </div>
+            </div>
+            <div
+              className="signin_to_signup"
+              onClick={(event) => {
+                this.props.signUpModalHandler();
+                this.props.signInModalHandler();
+              }}
+            >
+              이메일로 시작하기
             </div>
           </div>
-        )}
+        </div>
       </div>
     );
   }
